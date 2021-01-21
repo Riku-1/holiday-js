16f736864eaaeeda6bc22c8ae25c3a0a8651dc28
Riku-1 <osaka.riku@gmail.com>
2021-01-21 17:48:36 +0900

    Created isDayOff function

---
+++ src/Day.ts
@@ -0,0 +1,11 @@
+export const Day = {
+  Sun: 0,
+  Mon: 1,
+  Tue: 2,
+  Wed: 3,
+  Thu: 4,
+  Fri: 5,
+  Sat: 6,
+} as const;
+
+export type Day = typeof Day[keyof typeof Day];
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,27 @@
-console.log('aaa')
+import { Day } from './Day';
+
+class WDate {
+  constructor(private date: Date, private holidayList: HolidayList) {}
+  /**
+   * return whether the date is a day off or not
+   * TODO: check dayOffList
+   * @param date
+   * @returns
+   */
+  isDayOff(date: Date) {
+    return this.holidayList.weeklyHoliday.some(
+      (value) => date.getDay() === value
+    );
+  }
+
+  // 稼働日で何日後か
+
+  // 稼働日でx日かかると何日になるか
+}
+
+class HolidayList {
+  constructor(
+    dayOffList: Date[],
+    public weeklyHoliday: Day[] = [Day.Sat, Day.Sun]
+  ) {}
+}
