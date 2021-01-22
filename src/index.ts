fa1559b4a88a333f8eab7302531267631522f978
Riku-1 <osaka.riku@gmail.com>
2021-01-22 17:19:01 +0900

    Feature:Implemented to check dayOffList

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,26 @@
 import { Day } from './Day';
 
 export class WDate {
-  constructor(private date: Date, private holidayList: HolidayList) {}
+  constructor(private date: Date, private holidayList: HolidayList) {
+    // unify time to easy to compare
+    this.date.setHours(0, 0, 0, 0);
+  }
+
   /**
    * return whether the date is a day off or not
-   * TODO: check dayOffList
    * @param date
    * @returns
    */
   isDayOff() {
-    return this.holidayList.weeklyHoliday.some(
+    const isWeeklyHoliday = this.holidayList.weeklyHoliday.some(
       (value) => this.date.getDay() === value
+    );
+    if (isWeeklyHoliday) {
+      return true;
+    }
+
+    return this.holidayList.dayOffList.some(
+      (value) => this.date.getTime() === this.date.getTime()
     );
   }
 
@@ -21,7 +31,7 @@ export class WDate {
 
 export class HolidayList {
   constructor(
-    dayOffList: Date[] = [],
+    public dayOffList: Date[] = [],
     public weeklyHoliday: Day[] = [Day.Sat, Day.Sun]
   ) {}
 }
